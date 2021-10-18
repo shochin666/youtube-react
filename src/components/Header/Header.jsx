12da011bef48659ff7576870dae1c4ddd28d3751
github shochin666 <shoogawa111@gmail.com>
2021-10-18 16:45:10 +0900

    header completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,18 +4,19 @@ import { Box } from '@chakra-ui/layout'
 import { FormControl } from '@chakra-ui/form-control'
 import { Input } from '@chakra-ui/input'
 import { Button } from '@chakra-ui/button'
-
+import { Text } from "@chakra-ui/react"
+import { SearchIcon } from '@chakra-ui/icons'
 
 export const Header = () => {
     return (
-        <Box>
-            <Box>
-                <Link to="/">VideoTube</Link>
-            </ Box>
+        <Box pos="fixed" left="0" top="0" display="flex" height="10" width="100%" backgroundColor="gray.700">
+            <Box mt={2} mx={2} flex="1">
+                <Link to="/"><Text color="white">VideoTube</Text></Link>
+            </Box>
             
-            <FormControl id="search">
-                <Input type="text" placeholder="入力" />
-                <Button type="submit">検索</Button>
+            <FormControl id="search" display="flex" flex="2">
+                <Input type="text" placeholder="検索" textColor="white" width="50%" mt={1} height={8} />
+                <Button type="submit" backgroundColor="blackAlpha.600" width={20} mt={1} height={8}><SearchIcon color="white" /></Button>                
             </FormControl>
         </Box>
     )
